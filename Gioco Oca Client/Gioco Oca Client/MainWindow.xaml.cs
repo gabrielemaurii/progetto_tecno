b9ea7510665875b0e20240dc68889ad055865165
SUPERGABRY04 <96728867+SUPERGABRY04@users.noreply.github.com>
2023-01-16 22:32:37 +0100

    Add files via upload

---
+++ Gioco Oca Client/Gioco Oca Client/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gioco_Oca_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Frame x; private readonly object mainFrame;
+
+        //Name = "mainFrame" />
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+       
+        private void Bottone_di_inizio_gioco__Click(object sender, RoutedEventArgs e)
+        {
+
+
+            mainFrame(new Uri("Pagina1.xaml", UriKind.Relative));
+    
+
+        }
+    }
+}
